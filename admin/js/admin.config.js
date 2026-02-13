abffbd7bb6e363fdab0de8ff4ae5a2d3b6079050
CM94 <64427455+CM94ARG@users.noreply.github.com>
2026-02-13 03:35:13 -0300

    Update storage keys in admin configuration

--- admin/js/admin.config.js
+++ admin/js/admin.config.js
@@ -1,8 +1,8 @@
 const ADMIN_CONFIG = {
     API_URL: 'https://script.google.com/macros/s/AKfycbyO0ZJeyK1eUBI-9D8xTQyxge2sy_dV8iIXWEW5wDoKm5LWeASt0bfXKCgkj8joZ4PD/exec',
     STORAGE_KEYS: {
-        ZONA_ACTIVA: 'admin_zona_activa',           
-        DEVICE_REGISTERED: 'admin_device_registered', 
-        DEVICE_ID: 'admin_device_id'               
+        ZONA_GUARDADA: 'zona_guardada',                    
+        DEVICE_REGISTERED: 'heladerias_device_registered', 
+        DEVICE_ID: 'heladerias_device_id'                        
     }
 };
